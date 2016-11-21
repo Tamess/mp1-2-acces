b6fec1dd8c3dec6f583326e9f790cb75b6e0dbf8
Chaddaï Fouché <chaddai.fouche@gmail.com>
2016-11-21 19:09:26 +0100

    Résultat après séance du 21/11/2016

---
+++ exemple_CSV.py
@@ -0,0 +1,23 @@
+﻿# utiliser csv
+import os
+import csv
+
+# on travaillera dans un dossier qu'on choisira
+base_chemin = "W:/essai"
+os.chdir(base_chemin)
+# avec un fichier du nom suivant
+csv_chemin = "test.csv"
+
+# écrire un fichier csv
+with open(csv_chemin, mode='w', newline='') as csv_file :
+    csv_writer = csv.writer(csv_file,delimiter=';')
+
+    csv_writer.writerow(['Truc', 'Machin'])
+    csv_writer.writerow(['Bidule','MachinChose'])
+
+# lire un fichier csv
+with open(csv_chemin, mode='r', newline='') as csv_file :
+    csv_reader = csv.reader(csv_file,delimiter=';')
+
+    for ligne in csv_reader :
+        print(ligne)
