3d34079ddfe898070250fda337cc95d07af17409
Chaddai <chaddai.fouche@gmail.com>
2016-11-29 18:05:49 +0100

    Après séance 29/11

---
+++ projetgreg.py
@@ -0,0 +1,3 @@
+﻿nils = Image.open("Q:\Espace d'échange\Mini projets ISN\mp1-2-access\DELRIEU.jpg")
+corentin = Image.open("Q:\Espace d'échange\Mini projets ISN\mp1-2-access\LEFORT.jpg")
+greg = Image.open("Q:\Espace d'échange\Mini projets ISN\mp1-2-access\ROBAIRE.jpg")
